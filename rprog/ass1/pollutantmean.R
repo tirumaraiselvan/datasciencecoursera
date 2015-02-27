185e6ae313789d70c28eea20ad5ee180912b1761
tirumaraiselvan <tirumarai.selvan@gmail.com>
2015-02-27 13:16:01 +0530

    part 1

---
+++ rprog/ass1/pollutantmean.R
@@ -0,0 +1,26 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'pollutant' is a character vector of length 1 indicating
+  ## the name of the pollutant for which we will calculate the
+  ## mean; either "sulfate" or "nitrate".
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return the mean of the pollutant across all monitors list
+  ## in the 'id' vector (ignoring NA values)  
+  sum = 0.0
+  count = 0
+  for (i in id) {
+    
+    FilePath = paste(directory, sprintf("/%.3i.csv",i), sep="") 
+    ## print(FilePath)
+    data = read.csv(FilePath, header = TRUE )
+    sum = sum + sum(data[pollutant],na.rm =TRUE)
+    count = count +sum(!is.na(data[pollutant]))
+  }
+  
+  return(round(sum/count, digits =3)) 
+}
